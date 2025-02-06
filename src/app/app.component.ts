de577878ffac1409e7c76064c09d7c10de013aee
Alberto Basanta <dasantades@gmail.com>
2025-02-06 11:05:46 +0100

    css header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PreloadHyperspaceComponent } from './components/preload-hyperspace/preload-hyperspace.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/header/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, PreloadHyperspaceComponent],
+  imports: [RouterOutlet, HeaderComponent, PreloadHyperspaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [NavBarComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
