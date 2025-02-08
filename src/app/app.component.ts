8046f14fc92dc01d5f785c6d639252a57277cb67
Alberto Basanta <dasantades@gmail.com>
2025-02-08 11:48:29 +0100

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PreloadHyperspaceComponent } from './components/preload-hyperspace/preload-hyperspace.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, PreloadHyperspaceComponent],
+  imports: [RouterOutlet, HeaderComponent, PreloadHyperspaceComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
