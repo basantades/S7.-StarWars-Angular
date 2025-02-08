efb24fe4613c8b36622ad17614301e64fe7a4b0a
Alberto Basanta <dasantades@gmail.com>
2025-02-08 17:22:54 +0100

    add video component

--- src/app/components/list-starships/list-starships.component.ts
+++ src/app/components/list-starships/list-starships.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, HostListener } from '@angular/core';
 import { StarshipsService } from '../../services/starships.service';
-import { Title } from '@angular/platform-browser';
 import { TitleComponent } from "../title/title.component";
+import { VideoComponent } from '../video/video.component';
 
 @Component({
   selector: 'app-list-starships',
   templateUrl: './list-starships.component.html',
   styleUrl: './list-starships.component.scss',
-  imports: [TitleComponent]
+  imports: [TitleComponent, VideoComponent]
 })
 export class ListStarshipsComponent {
 
