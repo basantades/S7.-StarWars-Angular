778fd2796d05a4213be87b3009412c3bd439f308
Alberto Basanta <dasantades@gmail.com>
2025-02-08 19:58:31 +0100

    change routes y add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListStarshipsComponent } from './components/list-starships/list-starships.component';
 import { ShowStarshipComponent } from './components/show-starship/show-starship.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 
-    { path: '', component: ListStarshipsComponent },
+    { path: '', component: HomeComponent },
+    { path: 'starships', component: ListStarshipsComponent },
     { path: 'starship/:id', component: ShowStarshipComponent },
     { path: '**', redirectTo: '' } // Ruta de "no encontrado"
 ];
