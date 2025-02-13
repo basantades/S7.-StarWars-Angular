4cb65807935ec38f4515be14864af84ea0b65144
Alberto Basanta <dasantades@gmail.com>
2025-02-14 00:39:48 +0100

    hamburguer ok

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MenuUserComponent } from './menu-user/menu-user.component';
-import { HamburgerComponent } from './hamburger/hamburger.component';
 
 @Component({
   selector: 'app-header',
-  imports: [NavBarComponent, MenuUserComponent, HamburgerComponent],
+  imports: [NavBarComponent, MenuUserComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
