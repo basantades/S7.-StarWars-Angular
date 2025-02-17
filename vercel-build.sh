d1aaae62cb512cbc9c6857083eb12f84e37631c8
Alberto Basanta <dasantades@gmail.com>
2025-02-17 12:05:09 +0100

    vercel-build.sh

---
+++ vercel-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Creando environment.ts en Vercel..."
+
+cat <<EOT > src/environments/environment.ts
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '${NG_APP_FIREBASE_API_KEY}',
+    authDomain: '${NG_APP_FIREBASE_AUTH_DOMAIN}',
+    projectId: '${NG_APP_FIREBASE_PROJECT_ID}',
+    storageBucket: '${NG_APP_FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${NG_APP_FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${NG_APP_FIREBASE_APP_ID}'
+  }
+};
+EOT
