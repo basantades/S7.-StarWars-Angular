82a0d5216c8efe1beeb1d32d3f170cc50d671435
Alberto Basanta <dasantades@gmail.com>
2025-02-17 12:44:45 +0100

    vercel-build.sh change

--- vercel-build.sh
+++ vercel-build.sh
@@ -4,7 +4,7 @@ echo "Creando environment.ts en Vercel..."
 cat <<EOT > src/environments/environment.ts
 export const environment = {
   production: true,
-  firebase: {
+  firebaseConfig: {
     apiKey: '${NG_APP_FIREBASE_API_KEY}',
     authDomain: '${NG_APP_FIREBASE_AUTH_DOMAIN}',
     projectId: '${NG_APP_FIREBASE_PROJECT_ID}',
