5827d252e1543904becf7a45d8329548e133eadc
Alberto Basanta <dasantades@gmail.com>
2025-02-24 00:09:34 +0100

    Change the [routerLink] at the end of the animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { VideoComponent } from '../video/video.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [VideoComponent],
+  imports: [VideoComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
