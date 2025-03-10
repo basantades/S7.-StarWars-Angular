23500a33f6a210374913725de3e051445bfee4d2
Alberto Basanta <dasantades@gmail.com>
2025-03-10 12:36:46 +0100

    added environment.example.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,15 @@
+// change this to your firebase config and rename this file to environment.ts
+
+export const environment = {
+    production: false,
+
+ firebaseConfig: {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+}
+    
+  };
