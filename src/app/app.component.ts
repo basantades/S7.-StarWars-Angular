a33a76d656aef3630a2e2da56bdf7967ac464435
Alberto Basanta <dasantades@gmail.com>
2025-03-12 10:23:29 +0100

    changes mobile menu and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { RouterOutlet, Router, NavigationEnd  } from '@angular/router';
 import { PreloadHyperspaceComponent } from './components/preload-hyperspace/preload-hyperspace.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MenuUserComponent } from './menu-user/menu-user.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,25 @@ import { RouterLink } from '@angular/router';
 export class HeaderComponent {
   isMenuOpen = false;
 
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit() {
+    // Suscribirse a los eventos de navegación
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isMenuOpen = false; // Cerrar el menú al completar la navegación
+      }
+    });
+  }
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+  goBack() {
+    this.location.back(); 
+  }
+  // Método para verificar si estamos en la home
+  isHome(): boolean {
+    return this.router.url === '/';
   }
 }
